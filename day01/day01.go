187c15ca56d5cdb4afa09719b8c0bf2af150d872
Lawrence Foley <emwav333@gmail.com>
2020-12-17 19:28:56 -0600

    Add day 1 part 2.

--- day01/day01.go
+++ day01/day01.go
@@ -33,10 +33,41 @@ func main() {
 		_, present := complimentMap[currentNum]
 		if present {
 			answer := currentNum * complimentMap[currentNum]
-			fmt.Println(answer)
+			fmt.Printf("part 1: %d\n", answer)
 		} else {
 			// Set the key to the compliment so that we can find this value later
 			complimentMap[2020-currentNum] = currentNum
 		}
 	}
+
+	fmt.Printf("part 2: %d\n", part2(splitText))
+}
+
+func part2(splitText []string) int {
+	var nums = make([]int, 0)
+	var err error
+	var curVal int
+	for _, val := range splitText {
+		curVal, err = strconv.Atoi(val)
+		if err != nil {
+			// skip non-number lines
+			continue
+		}
+		nums = append(nums, curVal)
+	}
+
+	// Yeah, I know
+	var count int = 0
+	for _, val1 := range nums {
+		for _, val2 := range nums {
+			for _, val3 := range nums {
+				count++
+				if (val1 + val2 + val3) == 2020 {
+					return val1 * val2 * val3
+				}
+			}
+		}
+	}
+
+	return 0
 }
