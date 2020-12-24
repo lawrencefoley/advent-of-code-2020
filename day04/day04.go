8d8b07c2ecb52f8760850e2b555580e96a701330
Lawrence Foley <emwav333@gmail.com>
2020-12-24 15:42:08 -0600

    Add day 4 part 3.

--- day04/day04.go
+++ day04/day04.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 var recordKeyValueRegex *regexp.Regexp
+var heightRegex *regexp.Regexp
+var hairColorRegex *regexp.Regexp
+var eyeColorRegex *regexp.Regexp
+var passportIDRegex *regexp.Regexp
 
 func checkError(err error) {
 	if err != nil {
@@ -19,7 +24,7 @@ func readFileRecords(filename string) []string {
 	data, err := ioutil.ReadFile(filename)
 	checkError(err)
 
-	lines := strings.Split(string(data), "\n\n")
+	lines := strings.Split(string(data), "\r\n\r\n")
 	return lines
 }
 
@@ -44,6 +49,97 @@ func recordIsValid(record map[string]string, requiredFields *[]string) bool {
 	return true
 }
 
+func recordIsValidStrict(record map[string]string, requiredFields *[]string) bool {
+	if !recordIsValid(record, requiredFields) {
+		return false
+	}
+
+	for k, v := range record {
+		switch k {
+		case "byr":
+			if !validBirthYear(v) {
+				return false
+			}
+		case "iyr":
+			if !validIssueYear(v) {
+				return false
+			}
+		case "eyr":
+			if !validExpirationYear(v) {
+				return false
+			}
+		case "hgt":
+			if !validHeight(v) {
+				return false
+			}
+		case "hcl":
+			if !validHairColor(v) {
+				return false
+			}
+		case "ecl":
+			if !validEyeColor(v) {
+				return false
+			}
+		case "pid":
+			if !validPassportID(v) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func validBirthYear(val string) bool {
+	year, err := strconv.Atoi(val)
+	if err != nil {
+		return false
+	}
+	return year >= 1920 && year <= 2002
+}
+
+func validIssueYear(val string) bool {
+	year, err := strconv.Atoi(val)
+	if err != nil {
+		return false
+	}
+	return year >= 2010 && year <= 2020
+}
+
+func validExpirationYear(val string) bool {
+	year, err := strconv.Atoi(val)
+	if err != nil {
+		return false
+	}
+	return year >= 2020 && year <= 2030
+}
+
+func validHeight(val string) bool {
+	matches := heightRegex.FindAllSubmatch([]byte(val), -1)
+	if len(matches) < 1 || len(matches[0]) < 2 {
+		return false
+	}
+
+	height, _ := strconv.Atoi(string(matches[0][1]))
+	if string(matches[0][2]) == "cm" {
+		return height >= 150 && height <= 193
+	} else {
+		return height >= 59 && height <= 76
+	}
+}
+
+func validHairColor(val string) bool {
+	return hairColorRegex.Match([]byte(val))
+}
+
+func validEyeColor(val string) bool {
+	return eyeColorRegex.Match([]byte(val))
+}
+
+func validPassportID(val string) bool {
+	return passportIDRegex.Match([]byte(val))
+}
+
 func main() {
 	fmt.Println("day04")
 
@@ -52,7 +148,8 @@ func main() {
 
 	validRecords := 0
 	records := readFileRecords("input.txt")
-	// parseRecord := nil
+	fmt.Printf("%d records\n", len(records))
+
 	for _, record := range records {
 		parseRecord := parseRecord(record)
 		if recordIsValid(parseRecord, &requiredFields) {
@@ -61,4 +158,24 @@ func main() {
 	}
 
 	fmt.Printf("part 1: %d\n", validRecords)
+
+	// -------------------------------------
+	// Part 2
+	validRecordsPartTwo := 0
+	heightRegex = regexp.MustCompile(`(\d+)(cm|in)`)
+	hairColorRegex = regexp.MustCompile(`#[0-9a-f]{6}`)
+	eyeColorRegex = regexp.MustCompile(`(amb|blu|brn|gry|grn|hzl|oth)`)
+	passportIDRegex = regexp.MustCompile(`^\d{9}$`)
+
+	for _, record := range records {
+		parseRecord := parseRecord(record)
+		if recordIsValidStrict(parseRecord, &requiredFields) {
+			validRecordsPartTwo++
+			delete(parseRecord, "cid")
+			fmt.Printf("%s\n", parseRecord)
+		}
+	}
+
+	fmt.Printf("part 2: %d\n", validRecordsPartTwo)
+
 }
